3448a943ef51cbde327bdea37f7ae4a9a8389027
baroquebobcat <ndh@baroquebobcat.com>
2009-04-19 01:38:17 -0500

    changed passenger package to use debian conventions.

--- config/stack/apache.rb
+++ config/stack/apache.rb
@@ -18,28 +18,25 @@ end
 
 package :passenger, :provides => :appserver do
   description 'Phusion Passenger (mod_rails)'
-  version '2.1.3'
+  version '2.2.0'
   gem 'passenger' do
     post :install, 'echo -en "\n\n\n\n" | sudo passenger-install-apache2-module'
 
     # Create the passenger conf file
-    post :install, 'mkdir -p /etc/apache2/extras'
-    post :install, 'touch /etc/apache2/extras/passenger.conf'
-    post :install, 'echo "Include /etc/apache2/extras/passenger.conf"|sudo tee -a /etc/apache2/apache2.conf'
+    loading = %Q(LoadModule passenger_module /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}/ext/apache2/mod_passenger.so)
+    conf = %Q(PassengerRoot /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}
+PassengerRuby /usr/local/bin/ruby
+RailsEnv production)
 
-    [%Q(LoadModule passenger_module /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}/ext/apache2/mod_passenger.so),
-    %Q(PassengerRoot /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}),
-    %q(PassengerRuby /usr/local/bin/ruby),
-    %q(RailsEnv production)].each do |line|
-      post :install, "echo '#{line}' |sudo tee -a /etc/apache2/extras/passenger.conf"
-    end
-
+    post :install, "echo '#{conf}' >> /etc/apache2/mods-available/passenger.conf"
+    post :install, "echo '#{loading}' >> /etc/apache2/mods-available/passenger.load"
+    post :install, 'a2enmod passenger'
     # Restart apache to note changes
     post :install, '/etc/init.d/apache2 restart'
   end
 
   verify do
-    has_file "/etc/apache2/extras/passenger.conf"
+    has_file "/etc/apache2/mods-enabled/passenger.load"
     has_file "/usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}/ext/apache2/mod_passenger.so"
     has_directory "/usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}"
   end
